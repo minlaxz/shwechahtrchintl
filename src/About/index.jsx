e7d98f3573307c02f361cce5f8704ae91db1780e
Min Latt <minminlaxz@gmail.com>
2021-07-29 23:56:13 +0630

    About is finished
    hungrrrry 🥴

--- src/About/index.jsx
+++ src/About/index.jsx
@@ -21,7 +21,22 @@ const About = () => {
         <>
             <div className={`${styles.root_container}`}>
                 <h2 className="cyan-text">Hello, weed! 🥴</h2>
-                <code className="gold-text">ရွှေချထားချင်တယ်။ 🤪</code>
+                <p className="gold-text">ရွှေချထားချင်တယ်။ 🤪</p>
+                <div>
+                    <ul>
+                        <li>Created by <a href="https://minlaxz.me" rel="noreferrer noopener" target="_blank">minlaxz</a> and used...</li>
+                        <li><a rel="noreferrer noopener" target="_blank" href="https://vitejs.dev/">Vite</a> build tool FAST!</li>
+                        <li><a rel="noreferrer noopener" target="_blank" href="https://reactjs.org/">React JS</a></li>
+                        <li><a rel="noreferrer noopener" target="_blank" href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/">Flex</a> CSS</li>
+                        <li><a rel="noreferrer noopener" target="_blank" href="https://github.com/css-modules/css-modules">CSS Moudles</a></li>
+                        <li><a rel="noreferrer noopener" target="_blank" href="https://www.freenom.com/en/index.html?lang=en">Freenom</a>for free domain name</li>
+                        <li><a rel="noreferrer noopener" target="_blank" href="https://www.cloudflare.com/">Cloudflare</a> for SSL/TLS</li>
+                        <li><a rel="noreferrer noopener" target="_blank" href="https://github.com/minlaxz/shwechahtrchintl/blob/2393b1b79c1b7e73113860e47b562f011daf7028/CNAME#L1">CNAME and APEX</a> for DNS routings</li>
+                        <li><a rel="noreferrer noopener" target="_blank" href="https://pages.github.com/">Github Pages</a> for serving static files</li>
+                        <li><a rel="noreferrer noopener" target="_blank" href="https://docs.github.com/en/actions/learn-github-actions">Github Actions</a> for automated CI</li>
+                        <li><a rel="noreferrer noopener" target="_blank" href="https://reactrouter.com/web/api/HashRouter">HashRouter</a> for frontend routing like GH Pages</li>
+                    </ul>
+                </div>
                 <p className="white-text"> Here is the
                     <a href="https://github.com/minlaxz/shwechahtrchintl" rel="noreferrer noopener" target="_blank"
                         class="source-text"> Source Code
@@ -31,20 +46,20 @@ const About = () => {
                     </a>
 
                 </p>
-                <code> last sha : 
+                <code> sha :
                     {
-                        commitSha ? 
-                        `${commitSha}` : 'Fetching last commit SHA, please weed...'
+                        commitSha ?
+                            `${commitSha}` : 'Fetching last commit SHA, please weed...'
                     }
                 </code>
-                <code> last commit date : 
+                <code> last commit date :
                     {
                         commitDate ? `${commitDate}` : 'Fetching last commit date, please weed...'
                     }
                 </code>
                 {commitSha && <a a href={`https://github.com/minlaxz/shwechahtrchintl/commit/${commitSha}`} target="_blank" rel="noreferrer noopener">WTF is goin' on @sha</a>}
                 <br></br>
-                <Link to="/">Go back to Home 🏠</Link>
+                <Link to="/">This is complicated 🥴, just go back to Home 🏠</Link>
             </div>
 
         </>
